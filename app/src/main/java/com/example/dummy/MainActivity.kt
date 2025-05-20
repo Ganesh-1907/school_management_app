b54a19a95ccf452c8a166148358cdddc48c5c613
Ganesh <bora1132004@gmail.com>
2025-05-20 21:57:16 +0530

    update: java files

--- app/src/main/java/com/example/dummy/MainActivity.kt
+++ app/src/main/java/com/example/dummy/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.dummy
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,17 +17,24 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val spinner: Spinner = findViewById(R.id.roles_select_spinner_login_page)
+        val emailEditText = findViewById<EditText>(R.id.username_edit_text_login)
+        val passwordEditText = findViewById<EditText>(R.id.password_edittext_login)
+        val loginButton = findViewById<Button>(R.id.Login_button_login_page)
 
-        val adapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.login_roles_array,
-            android.R.layout.simple_spinner_item
-        )
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = adapter
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter)
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            // Print to Logcat
+            Log.d("LoginData", "Email: $email, Password: $password")
+
+            // Show in a Toast popup
+            Toast.makeText(this, "Email: $email\nPassword: $password", Toast.LENGTH_LONG).show()
+            // Navigate to OfficerDashboardActivity
+            val intent = Intent(this, Officer_dashboard::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
