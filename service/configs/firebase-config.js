0c729dc77a9c0b96c00396992ef9aa995775477b
Ganesh <bora1132004@gmail.com>
2025-05-20 21:55:42 +0530

    update paths of files

--- service/configs/firebase-config.js
+++ service/configs/firebase-config.js
@@ -1,13 +1,21 @@
-// firebase.js
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import admin from "firebase-admin";
-import serviceAccount from "../serviceAccountKey.json" assert { type: "json" };
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const serviceAccount = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../serviceAccountKey.json"), "utf-8")
+);
 
 dotenv.config();
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIRE_BASE_db_url,
+  databaseURL: process.env.FIREBASE_DB_URL,
 });
 
 const db = admin.firestore();
